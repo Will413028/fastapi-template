be5a1eecc919eb8d34e3f6204695a2f364ddba87
Will413028 <will413028@gmail.com>
2024-05-16 11:01:13 +0800

    📝 Update docs

--- src/auth/router.py
+++ src/auth/router.py
@@ -20,6 +20,7 @@ from src.auth.service import (
     get_user_info,
 )
 from src.config import settings
+from src.constans import common_responses
 from src.database import get_db
 from src.logger import stru_logger
 
@@ -128,46 +129,7 @@ def login(
 
 @router.get(
     "/user/info",
-    responses={
-        status.HTTP_400_BAD_REQUEST: {
-            "model": dict[str, str],
-            "content": {
-                "application/json": {
-                    "example": {"detail": "An error message explaining the specific issue"}
-                }
-            },
-            "description": "An error message explaining the specific issue.",
-        },
-        status.HTTP_401_UNAUTHORIZED: {
-            "model": dict[str, str],
-            "content": {
-                "application/json": {
-                    "example": {"detail": "Unauthorized - The request requires user authentication"}
-                }
-            },
-        },
-        status.HTTP_403_FORBIDDEN: {
-            "model": dict[str, str],
-            "content": {
-                "application/json": {
-                    "example": {
-                        "detail": "Permission denied - "
-                        "The client does not have permission to access the requested resource"
-                    }
-                }
-            },
-        },
-        status.HTTP_422_UNPROCESSABLE_ENTITY: {
-            "model": dict[str, str],
-            "content": {
-                "application/json": {
-                    "example": {
-                        "detail": "Invalid input data. Please check the input fields for errors."
-                    }
-                }
-            },
-        },
-    },
+    responses=common_responses,
     response_model=UserInfo,
     tags=["user"],
 )
---
+++ src/constans.py
@@ -0,0 +1,41 @@
+from fastapi import status
+
+common_responses = {
+    status.HTTP_400_BAD_REQUEST: {
+        "model": dict[str, str],
+        "content": {
+            "application/json": {
+                "example": {"detail": "An error message explaining the specific issue."}
+            }
+        },
+    },
+    status.HTTP_401_UNAUTHORIZED: {
+        "model": dict[str, str],
+        "content": {
+            "application/json": {
+                "example": {"detail": "Unauthorized - The request requires user authentication"}
+            }
+        },
+    },
+    status.HTTP_403_FORBIDDEN: {
+        "model": dict[str, str],
+        "content": {
+            "application/json": {
+                "example": {
+                    "detail": "Permission denied - "
+                    "The client does not have permission to access the requested resource"
+                }
+            }
+        },
+    },
+    status.HTTP_422_UNPROCESSABLE_ENTITY: {
+        "model": dict[str, str],
+        "content": {
+            "application/json": {
+                "example": {
+                    "detail": "Invalid input data. Please check the input fields for errors."
+                }
+            }
+        },
+    },
+}
